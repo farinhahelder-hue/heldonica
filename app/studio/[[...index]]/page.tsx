ef9b01d1d84e71287a99d97bb0ca5e27385d0330
Heldonica <heldonica@farinhahelder.com>
2026-03-25 05:23:31 -0400

    Fix Sanity Studio dynamic import

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,7 +1,12 @@
 'use client'
 
-import { Studio } from 'sanity'
+import dynamic from 'next/dynamic'
 import { config } from '@/sanity.config'
+
+const Studio = dynamic(() => import('sanity').then(mod => ({ default: mod.Studio })), {
+  ssr: false,
+  loading: () => <div className="flex items-center justify-center min-h-screen">Chargement du Studio...</div>,
+})
 
 export default function StudioPage() {
   return <Studio config={config} />
